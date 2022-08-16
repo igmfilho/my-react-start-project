1f86a20ca5ba0edb7090f5f7d5e3f6221d3ff950
Ivan Filho <igmfilho@gmail.com>
2022-08-16 15:54:45 +0100

    Alternative Function Syntax: arrow function

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -4,7 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import './ExpenseList.css'
 
-function ExpenseList(props) {
+const ExpenseList = (props) => {
     const expenses = props.items;
     const expenseItens = expenses.map((item) => 
         <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}></ExpenseItem>
