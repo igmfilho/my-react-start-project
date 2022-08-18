d7fbb2cccb98626b3fd0bb3e55aa98319cda82ad
Ivan Filho <igmfilho@gmail.com>
2022-08-18 21:50:11 +0100

    Updating State That Depends On The Previous State

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,37 +3,46 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    });
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // });
 
     const titleHandlerChange = event => {
-        //setEnteredTitle(event.target.value);
-        setUserInput({
+        setEnteredTitle(event.target.value);
+        /* setUserInput({
             ...userInput,
             enteredTitle: event.target.value,
-        });
+        }); */
+        // setUserInput((prevState)=> {
+        //     return { ...prevState, enteredTitle: event.target.value };
+        // });
     }
 
     const amountHandlerChange = event => {
-        //setEnteredAmount(event.target.value);
-        setUserInput({
+        setEnteredAmount(event.target.value);
+        /* setUserInput({
             ...userInput,
             enteredAmount: event.target.value,
-        });
+        }); */
+        // setUserInput((prevState)=> {
+        //     return { ...prevState, enteredAmount: event.target.value };
+        // });
     }
 
     const dateHandlerChange = event => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
+        setEnteredDate(event.target.value);
+       /*  setUserInput({
             ...userInput,
             enteredDate: event.target.value,
-        });
+        }); */
+        // setUserInput((prevState)=> {
+        //     return { ...prevState, enteredDate: event.target.value };
+        // });
     }
 
   return (
