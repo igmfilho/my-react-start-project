a06e6eb36c58f12e9ca7a40fc8fd91f0ac8bdb13
Ivan Filho <igmfilho@gmail.com>
2022-08-21 21:54:23 +0100

    Outputting conditional content

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -17,20 +17,24 @@ const ExpenseList = (props) => {
   }
   );
 
+  let expensesContent = <p>No expenses found.</p>;
+  if(filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      {filteredExpenses.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))}
+      {expensesContent}
     </Card>
   );
 };
