f00e82a9fb8b6f6209a09d27da66322681fe7183
Ivan Filho <igmfilho@gmail.com>
2022-08-21 18:46:40 +0100

    Assignment 2: Adding ExpensesFilter component

--- src/components/Expenses/ExpenseList.js
+++ src/components/Expenses/ExpenseList.js
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
-import './ExpenseList.css'
+import "./ExpenseList.css";
+import ExpensesFilter from "./ExpensesFilter";
 
 const ExpenseList = (props) => {
-    const expenses = props.items;
-    const expenseItens = expenses.map((item) => 
-        <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date}></ExpenseItem>
-    );
-    return (
-        <Card className="expenses">
-            {expenseItens}
-        </Card>
-    );
-}
+  const expenses = props.items;
+  const expenseItens = expenses.map((item) => (
+    <ExpenseItem
+      key={item.id}
+      title={item.title}
+      amount={item.amount}
+      date={item.date}
+    ></ExpenseItem>
+  ));
+
+  const [filteredYear, setFilteredYear] = useState("2022");
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      {expenseItens}
+    </Card>
+  );
+};
 
 export default ExpenseList;
